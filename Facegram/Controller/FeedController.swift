b9e11e86ba0f3faf696c8d8aace8e13b846be107
Rutul Dave <rutul98@yahoo.com>
2016-02-02 14:28:15 -0500

    Create the FeedController and add test Post to appear on load

---
+++ Facegram/Controller/FeedController.swift
@@ -0,0 +1,51 @@
+//
+//  FeedController.swift
+//  Facegram
+//
+//  Created by Rutul Dave on 2/2/16.
+//  Copyright © 2016 Rutul Dave. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UITableViewController {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        if let feed = Post.feed {
+            return feed.count
+        } else {
+            return 0
+        }
+    }
+    
+    func postIndex(cellIndex: Int) -> Int {
+        return tableView.numberOfSections - cellIndex - 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let post = Post.feed![postIndex(indexPath.section)]
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+        cell.captionLabel.text = post.caption
+        cell.imgView.image = post.image
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let post = Post.feed![postIndex(indexPath.section)]
+        if let img = post.image {
+            let aspectRation = img.size.height / img.size.height
+            return tableView.frame.size.width * aspectRation + 80 // height accounting for buttons and caption
+        }
+        
+        return 208 // default height
+    }
+}
