3bd4eb137c313b7844de0d8471271aeb60e48af8
Rutul Dave <rutul98@yahoo.com>
2016-02-01 22:10:41 -0500

    first commit: initialize project and create basic storyboard

---
+++ Facegram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facegram
+//
+//  Created by Rutul Dave on 2/1/16.
+//  Copyright © 2016 Rutul Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
